1bef865778dafce435ba0659c5a89fe6b876f37f
Tung Nguyen <shodydosh@gmail.com>
2023-10-04 11:18:08 +0700

    feat: Adding getUserById, updateUserById

--- src/main/java/com/javaBTL/BE_JAVA_BTL/controller/UserController.java
+++ src/main/java/com/javaBTL/BE_JAVA_BTL/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.javaBTL.BE_JAVA_BTL.controller;
 
 import com.javaBTL.BE_JAVA_BTL.model.User;
+import com.javaBTL.BE_JAVA_BTL.repository.UserRepository;
 import com.javaBTL.BE_JAVA_BTL.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -14,6 +17,9 @@ import java.util.UUID;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/add")
     public String add(@RequestBody User user){
@@ -24,17 +30,6 @@ public class UserController {
     @GetMapping("/all")
     public List<User> getAllUser() {
         return userService.getAllUser();
-    }
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable String userId) {
-        try {
-            UUID uuid = UUID.fromString(userId);
-            User user = userService.getUserById(uuid);
-            return ResponseEntity.ok(user);
-        } catch (IllegalArgumentException e) {
-            // Handle the case where the userId is not a valid UUID
-            return ResponseEntity.badRequest().build();
-        }
     }
     @GetMapping("")
     public ResponseEntity<User> getUserById(@RequestParam("id") UUID userId) {
@@ -49,4 +44,41 @@ public class UserController {
         }
     }
 
+    @DeleteMapping("")
+    public ResponseEntity<String> deleteUser(@RequestParam("id") UUID userId) {
+        try {
+            userService.deleteUser(userId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("User deleted successfully.");
+        } catch (IllegalArgumentException e) {
+            // Handle the case when the user with the provided ID does not exist
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> updateUser(@RequestParam("id") UUID userId, @RequestBody User updatedUser) {
+        try {
+            // Find the existing user by ID
+            Optional<User> existingUserOptional = userRepository.findById(userId);
+
+            if (existingUserOptional.isPresent()) {
+                User existingUser = existingUserOptional.get();
+
+                // Update the existing user with new info
+                if(updatedUser.getName() != null) existingUser.setName(updatedUser.getName());
+                if(updatedUser.getEmail() != null) existingUser.setEmail(updatedUser.getEmail());
+                if(updatedUser.getPassword() != null) existingUser.setPassword(updatedUser.getPassword());
+                // You can update other fields as needed
+
+                // Save the updated user back to the repository
+                User updatedUserResult = userRepository.save(existingUser);
+                return ResponseEntity.ok(updatedUserResult);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
+
