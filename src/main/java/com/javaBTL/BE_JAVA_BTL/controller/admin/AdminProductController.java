d7f1b0aa2f4ffbfc077d22508b0a4e157f0c997c
Duythong <Duythong.ptit@gmail.com>
2023-10-14 23:23:45 +0700

    Split Admin and Client controller in Product Controller

---
+++ src/main/java/com/javaBTL/BE_JAVA_BTL/controller/admin/AdminProductController.java
@@ -0,0 +1,55 @@
+package com.javaBTL.BE_JAVA_BTL.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.javaBTL.BE_JAVA_BTL.model.Product;
+import com.javaBTL.BE_JAVA_BTL.service.ProductService;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/api/product")
+public class AdminProductController {
+
+    @Autowired
+    private ProductService productService;
+    //Add 1 product
+    @PostMapping ("/add")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+    // Upload multi product
+    @PostMapping("/upload")
+    public List<Product> uploadProducts(@RequestBody List<Product> products) {
+        return productService.uploadProducts(products);
+    }
+
+    // Admin CRUD operations
+    @PostMapping("/{id}")
+    public Product createOrUpdateProduct(@PathVariable UUID id, @RequestBody Product product) {
+        // Kiểm tra xem sản phẩm với ID đã cho có tồn tại hay không
+        Product existingProduct = productService.getProductById(id);
+
+        if (existingProduct == null) {
+            // Nếu sản phẩm không tồn tại, tạo một sản phẩm mới với ID đã cho
+            // và lưu nó vào cơ sở dữ liệu
+            product.setId(id); // Đặt ID của sản phẩm
+            return productService.createProduct(product);
+        } else {
+            // Cập nhật thông tin sản phẩm từ product
+            existingProduct.setRetailer(product.getRetailer());
+            existingProduct.setImg_url(product.getImg_url());
+            existingProduct.setName(product.getName());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setUrl(product.getUrl());
+            existingProduct.setCategory(product.getCategory());
+
+            // Lưu sản phẩm đã cập nhật vào cơ sở dữ liệu
+            return productService.createOrUpdateProduct(id, existingProduct);
+        }
+    }
+    @DeleteMapping("delete/{id}")
+    public void deleteProduct(@PathVariable UUID id) {
+        productService.deleteProduct(id);
+    }
+}
