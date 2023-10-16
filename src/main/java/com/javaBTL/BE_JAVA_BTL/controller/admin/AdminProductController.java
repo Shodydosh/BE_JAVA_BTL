8e8c068e85788e6c5012a96bf515711901241436
Duythong <Duythong.ptit@gmail.com>
2023-10-16 19:02:03 +0700

    change  "/admin/api/product" to "/api/admin/product"

--- src/main/java/com/javaBTL/BE_JAVA_BTL/controller/admin/AdminProductController.java
+++ src/main/java/com/javaBTL/BE_JAVA_BTL/controller/admin/AdminProductController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/admin/api/product")
+@RequestMapping("/api/admin/product")
 public class AdminProductController {
 
     @Autowired
